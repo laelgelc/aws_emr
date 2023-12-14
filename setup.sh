a1cb29f6fccfe66d880ccffb7d0eb33b7e25c5e0
eyamrog <eyamrog@gmail.com>
2023-12-14 10:49:42 -0300

    20231214-1049

--- setup.sh
+++ setup.sh
@@ -14,23 +14,23 @@ update () {
 }
 
 venv () {
-    sudo apt install python3-pip
-    sudo apt install python3-venv
-    sudo apt install awscli
+    sudo apt install -y python3-pip
+    sudo apt install -y python3-venv
+    sudo apt install -y awscli
     python3 -m venv my_env
     source /home/ubuntu/my_env/bin/activate
-    pip install -r env.req # Make sure the file 'env.req' contains the requirements of the environment
 }
 
 git () {
     cd /home/ubuntu/my_env
     git clone https://github.com/laelgelc/gelc.git # Update the git repository link accordingly
     cd /home/ubuntu/my_env/gelc
+    pip install -r env.req # Make sure the file 'env.req' contains the requirements of the environment
     aws s3 cp s3://gelc/.env01 .env # Update the reference to the '.env' file accordingly
 }
 
 clear
 
 #update
-#venv
-#git
+venv
+git
